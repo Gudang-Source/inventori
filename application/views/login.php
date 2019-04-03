3d22c409389131d0031bcfe3bd2416eeb2fb7ea3
maillumau <mail.lumau@gmail.com>
2019-04-03 22:58:12 +0700

    Design Login

--- application/views/login.php
+++ application/views/login.php
@@ -1,52 +1,100 @@
-<?php $this->load->view('admin/components/header'); ?>
-<link href="<?php echo base_url(); ?>asset/css/animation.css" rel="stylesheet">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Inventroy</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->  
+    <link href="<?php echo base_url(); ?>asset/login/images/icons/favicon.ico" rel="stylesheet">
+<!--===============================================================================================-->
+    <link href="<?php echo base_url(); ?>asset/login/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<!--===============================================================================================-->
+    <link href="<?php echo base_url(); ?>asset/login/fonts/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet">
+<!--===============================================================================================-->
+    <link href="<?php echo base_url(); ?>asset/login/fonts/Linearicons-Free-v1.0.0/icon-font.min.css" rel="stylesheet">
+<!--===============================================================================================-->
+    <link href="<?php echo base_url(); ?>asset/login/vendor/animate/animate.css" rel="stylesheet">
+<!--===============================================================================================-->
+    <link href="<?php echo base_url(); ?>asset/login/vendor/css-hamburgers/hamburgers.min.css" rel="stylesheet">  
+<!--===============================================================================================-->
+    <link href="<?php echo base_url(); ?>asset/login/vendor/animsition/css/animsition.min.css" rel="stylesheet">
+<!--===============================================================================================-->
+    <link href="<?php echo base_url(); ?>asset/login/vendor/select2/select2.min.css" rel="stylesheet">
+<!--===============================================================================================-->
+    <link href="<?php echo base_url(); ?>asset/login/vendor/daterangepicker/daterangepicker.css" rel="stylesheet">  
+<!--===============================================================================================-->
+    <link href="<?php echo base_url(); ?>asset/login/css/util.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>asset/login/css/main.css" rel="stylesheet">
+<!--===============================================================================================-->
+</head>
+<body>
+    <?php
+    $error = $this->session->userdata('error');
+    if (!empty($error)) {
+        ?>
+        <div class="alert alert-danger"><?php
+            echo $error;
+            ?></div>
+    <?php }$this->session->unset_userdata('error'); ?>
+    
+    <div class="limiter">
+        <div class="container-login100">
+            <div class="wrap-login100 p-b-160 p-t-50">
+                <form class="login100-form validate-form" role="form" action="<?php echo base_url() ?>login" method="post">
+   
 
-<body class="login-page">
+                    <span class="login100-form-title p-b-43">
+                        Inventory
+                    </span >
+                    <span class="login100-form-title p-t-23">
+                    <?php echo validation_errors(); ?>
+                    <?php echo $this->session->flashdata('error'); ?>
+                    </span>
+                    <br>
+                    <div class="wrap-input100 rs1 validate-input" data-validate = "Username is required">
+                        <input class="input100" type="text" name="user_name">
+                        <span class="label-input100">Username</span>
+                    </div>
+                    
+                    
+                    <div class="wrap-input100 rs2 validate-input" data-validate="Password is required">
+                        <input class="input100" type="password" name="password">
+                        <span class="label-input100">Password</span>
+                    </div>
 
-<?php
-$error = $this->session->userdata('error');
-if (!empty($error)) {
-    ?>
-    <div class="alert alert-danger"><?php
-        echo $error;
-        ?></div>
-<?php }$this->session->unset_userdata('error'); ?>
-
-<div class="login-box">
-    <div class="login-logo animated fadeInDown" data-animation="fadeInDown">
-        <a href="<?php echo base_url() ?>"><b>Easy</b>Inventory </a>
-    </div><!-- /.login-logo -->
-    <div class="login-box-body  animated fadeInUp" data-animation="fadeInUp">
-        <p class="login-box-msg">Sign in to start your session</p>
-        <form role="form" action="<?php echo base_url() ?>login" method="post">
-            <?php echo validation_errors(); ?>
-            <?php echo $this->session->flashdata('error'); ?>
-            <div class="form-group has-feedback">
-                <input type="text" name="user_name" class="form-control" placeholder="Username" required="required" />
-                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                    <div class="container-login100-form-btn">
+                        <button class="login100-form-btn" type="submit" name="submit">
+                            Sign in
+                        </button>
+                    </div>
+                    
+                    <div class="text-center w-full p-t-23">
+                        <!-- <a href="<?php // echo base_url()?>forget_password" class="txt1">
+                            Forgot password?
+                        </a> -->
+                    </div>
+                </form>
             </div>
-            <div class="form-group has-feedback">
-                <input type="password" name="password" class="form-control" placeholder="Password" required="required" />
-                <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-            </div>
-            <div class="form-group has-feedback">
-                <button type="submit" class="btn bg-orange btn-block btn-flat">Login</button>
-            </div>
-            <div class="row">
-
-                <div class="col-xs-12">
-
-                </div><!-- /.col -->
-            </div>
-        </form>
-
-
-
-        <a href="<?php echo base_url()?>forget_password">I forgot my password</a><br>
-
-    </div><!-- /.login-box-body -->
-</div><!-- /.login-box -->
-
+        </div>
+    </div>
+    
+    
+    <!--===============================================================================================-->
+    <script src="<?php echo base_url() ?>asset/login/vendor/jquery/jquery-3.2.1.min.js" ></script>
+    <!--===============================================================================================-->
+    <script src="<?php echo base_url() ?>asset/login/vendor/animsition/js/animsition.min.js" ></script>
+    <!--===============================================================================================-->
+    <script src="<?php echo base_url() ?>asset/login/vendor/bootstrap/js/popper.js" ></script>
+    <script src="<?php echo base_url() ?>asset/login/vendor/bootstrap/js/bootstrap.min.js" ></script>
+    <!--===============================================================================================-->
+    <script src="<?php echo base_url() ?>asset/login/vendor/select2/select2.min.js" ></script>
+    <!--===============================================================================================-->
+    <script src="<?php echo base_url() ?>asset/login/vendor/daterangepicker/moment.min.js" ></script>
+    <script src="<?php echo base_url() ?>asset/login/vendor/daterangepicker/daterangepicker.js" ></script>
+    <!--===============================================================================================-->
+    <script src="<?php echo base_url() ?>asset/login/vendor/countdowntime/countdowntime.js" ></script>
+    <!--===============================================================================================-->
+    <script src="<?php echo base_url() ?>asset/login/js/main.js" ></script>
 
 </body>
 </html>
