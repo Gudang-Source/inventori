9a5cf3fafe7e9a17b84cde4b5a575acad7f09f6a
maillumau <mail.lumau@gmail.com>
2019-04-25 04:34:42 +0700

    satkai

--- application/controllers/admin/order.php
+++ application/controllers/admin/order.php
@@ -89,7 +89,7 @@ class Order extends Admin_Controller
 
             foreach ($customer_details as $customer) {
 
-                $satkai.="<option value='" . $customer->customer_id. "'>" .$customer->customer_name."</option>";
+                $satkai.="<option value='" . $customer->customer_id. "'>" .$customer->nama."</option>";
                 
             }
         }
